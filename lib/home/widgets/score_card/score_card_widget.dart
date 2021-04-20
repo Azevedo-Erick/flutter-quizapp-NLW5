c26596646c84cc1269a0b2d812dc6774cd4bea5a
Erick Azevedo <azevedoerick762@gmail.com>
2021-04-20 18:14:10 -0300

    adicao da tela de quiz e conclusao da homepage

--- lib/home/widgets/score_card/score_card_widget.dart
+++ lib/home/widgets/score_card/score_card_widget.dart
@@ -13,8 +13,10 @@ class ScoreCardWidget extends StatelessWidget {
           height: 136,
           decoration: BoxDecoration(
               color: AppColors.white, borderRadius: BorderRadius.circular(15)),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24),
+            child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Expanded(
@@ -39,6 +41,7 @@ class ScoreCardWidget extends StatelessWidget {
                   ))
             ],
           ),
+          )
         ));
   }
 }
